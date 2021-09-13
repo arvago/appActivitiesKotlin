0c2906adc8b6b6881f4c84639cdc5f41fc1b36e3
Arturo Vallejo González <71914027+arvago@users.noreply.github.com>
2021-09-12 19:04:13 -0500

    InfoFragment

--- app/src/main/java/com/example/practicaactivities/InfoFragment.kt
+++ app/src/main/java/com/example/practicaactivities/InfoFragment.kt
@@ -1,59 +1,76 @@
 package com.example.practicaactivities
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [InfoFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class InfoFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+class InfoFragment : Fragment(R.layout.fragment_info) {
+    override fun onResume() {
+        super.onResume()
+        setView()
+    }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+    private lateinit var ivPictureInfo: ImageView
+    private lateinit var txvInfo: TextView
+    private lateinit var ivFavorite: ImageView
+    private lateinit var picture: Picture
+    private var favFlag: Boolean = false
+
+    private fun setView(){
+        picture = requireArguments().getParcelable("selectedImage") ?: Picture()
+        ivPictureInfo = requireView().findViewById(R.id.ivPictureInfo)
+        txvInfo = requireView().findViewById(R.id.txvInfo)
+        ivFavorite = requireView().findViewById(R.id.ivFavorite)
+
+        ivPictureInfo.setImageResource(picture.source)
+        txvInfo.text = picture.title + ": " + picture.description
+        if(picture.favorite == false){
+            ivFavorite.setImageResource(R.drawable.estrella)
+        }else{
+            ivFavorite.setImageResource(R.drawable.estrella_rellena)
+        }
+
+        ivPictureInfo.setOnClickListener{
+            showImage()
+        }
+        ivFavorite.setOnClickListener {
+            addToFavorites()
         }
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?,
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_info, container, false)
+    private fun showImage(){
+        (requireActivity() as MainActivity).replaceFragment(ImageFragment().apply {
+            arguments = Bundle().apply {
+                putParcelable("imageSource", picture)
+            }
+        })
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment InfoFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            InfoFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+    private fun addToFavorites(){
+        if(picture.favorite == false && !favFlag){
+            ivFavorite.setImageResource(R.drawable.estrella_rellena)
+            favFlag = true
+
+            Picture.pictures.forEach {
+                when(it.id) {
+                    picture.id -> it.favorite = true
                 }
             }
+        }else{
+            ivFavorite.setImageResource(R.drawable.estrella)
+            favFlag = false
+
+            Picture.pictures.forEach {
+                when(it.id) {
+                    picture.id -> it.favorite = false
+                }
+            }
+        }
     }
 }
